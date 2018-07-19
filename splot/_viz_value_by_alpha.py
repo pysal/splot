33e7df4961ae1fa2e7dca8d72076a1df003a6a42
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2018-07-19 11:13:29 -0700

    add `_viz_value_by_alpha.py` functionality

--- splot/_viz_value_by_alpha.py
+++ splot/_viz_value_by_alpha.py
@@ -1,6 +1,8 @@
 import geopandas as gpd
 from shapely.geometry import Polygon
 import matplotlib
+import collections
+import matplotlib.cm as cm
 
 """
 Creating Value by Alpha maps
@@ -10,27 +12,16 @@ Creating Value by Alpha maps
 __author__ = ("Stefanie Lumnitz <stefanie.lumitz@gmail.com>")
 
 
-def value_by_alpha(x, alphay, gdf):
-    
+def value_by_alpha(x, y, gdf, cmap='GnBu'):
+    # option to put in other 
+    if isinstance(cmap, collections.Sequence):
+        cmap = colors.LinearSegmentedColormap.from_list('cmap', cmap)
+    cmap = cm.get_cmap(cmap)
+    rgba = cmap(x)
+    rgba[:, 3] = y/y.max()
+    return rgba
 
+def choropleth_vba(gdf, rgba)
+    ax = gdf.plot(color=rgba)
+    return ax
 
-
-def value_by_alpha(x='attribute', y='alpha_attribute', c, data=geodataframe):
-    rgb = matplotlib.colors.to_rgba_array(c)
-    alpha_channel = x/y.max()
-    rgba = np.vstack((rgb, np.asarray(alpha_channel).reshape(-1,1)))
-    fig, ax = geodataframe.plot(x, color=rgba)
-    return fig, ax
-
-def make_RGBA_array_from_attribute(x):
-    
-
-"""
-cols = matplotlib.colors.to_rgba_array(['r', 'r', 'r', 'r'])
-
-cols[:, -1] = [0.5, 0.7, 0.9, 0.2]
-
-s = geopandas.GeoSeries([Polygon([(random.random(), random.random()) for i in range(3)]) for _ in range(4)])
-
-s.plot(color=cols)
-"""
