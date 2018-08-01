2f3173882f91060fabda62adcd822083a3c85818
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2018-07-31 21:05:37 -0700

    adding `mappclassify_bin` functionality
    demonstrate binning of values for vba choropleth in notebook

--- splot/_viz_value_by_alpha_mpl.py
+++ splot/_viz_value_by_alpha_mpl.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 import collections
 import matplotlib.cm as cm
+from pysal.esda.mapclassify import (Box_Plot,
+                                    Quantiles,
+                                    Equal_Interval,
+                                    Fisher_Jenks)
 
 """
 Creating Value by Alpha maps
@@ -39,8 +43,9 @@ def value_by_alpha_cmap(x, y, cmap='GnBu', divergent=False):
         cmap = cm.get_cmap(cmap)
     elif isinstance(cmap, collections.Sequence):
         cmap = colors.LinearSegmentedColormap.from_list('newmap', cmap)
-    rgba = cmap(x)
-    rgba[:, 3] = y/y.max()
+    
+    rgba = cmap((x - x.min()) / (x.max() - x.min()))
+    rgba[:, 3] = (y - y.min()) / (y.max() - y.min())
     if divergent is not False:
         a_under_0p5 = rgba[:, 3] < 0.5
         rgba[a_under_0p5, 3] = 1 - rgba[a_under_0p5, 3]
@@ -94,4 +99,40 @@ def vba_choropleth(x, y, gdf, cmap='GnBu', divergent=False, ax=None):
     return fig, ax
 
 
-# def choropleth(gdf, x, y=None, cmap=None, classification=None, vba=None, divergent_vba=None, )
+def mapclassify_bin(y, classifier, k=5, hinge=1.5):
+    """
+    
+    Parameters
+    ----------
+    y : array
+        (n,1), values to classify
+    classifier : str
+        pysal.mapclassify classification scheme
+    k : int, optional
+        The number of classes.
+    hinge : float, optional
+        Multiplier for IQR when `Box_Plot` classifier used.
+    
+    Returns
+    -------
+    bins : pysal.mapclassify object
+        A mapclassify object with attributes:
+        yb (bin id for eachobservation),
+        bins (array of upper bounds of each class),
+        k (number of classes)
+        counts (number of observations in each class)
+    """
+    schemes = {}
+    schemes['equal_interval'] = Equal_Interval
+    schemes['quantiles'] = Quantiles
+    schemes['fisher_jenks'] = Fisher_Jenks
+    schemes['box_plot'] = Box_Plot
+    classifier = classifier.lower()
+    if classifier not in schemes:
+        raise ValueError("Invalid scheme. Scheme must be in the"
+                         " set: %r" % schemes.keys())
+    if classifier == 'box_plot':
+        bins = schemes[classifier](y, hinge)
+    else:
+        bins = schemes[classifier](y, k)
+    return bins
