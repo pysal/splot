adf61f56d6f6a018d15bbfb446e187d73ea29fc6
Stefanie Lumnitz <stefanie.lumnitz@gmail.com>
2018-08-07 10:48:01 -0700

    add color utilities `shift_colormap` and `truncate_colormap` to `splot.mapping`

--- splot/mapping.py
+++ splot/mapping.py
@@ -4,16 +4,31 @@
 
 Provides Choropleth visualizations and mapping utilities.
 
-Directional LISA analytics
---------------------------
+Value-by-Alpha maps
+-------------------
 
 .. autosummary::
    :toctree: generated/
 
    value_by_alpha_cmap
    vba_choropleth
+   vba_legend
+
+
+Colormap utilities
+------------------
+
+.. autosummary::
+   :toctree: generated/
+
+   shift_colormap
+   truncate_colormap
 
 """
 
 from ._viz_value_by_alpha_mpl import (value_by_alpha_cmap,
-                                      vba_choropleth)
+                                      vba_choropleth,
+                                      vba_legend)
+
+from ._viz_utils import (shift_colormap,
+                         truncate_colormap)
